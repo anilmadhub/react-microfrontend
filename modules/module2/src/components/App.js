36ef5c570d0dc7f392cab91769a0341b9db32b5b
anilmadhub <a.nilmadhub@gmail.com>
2020-09-25 21:04:29 +0400

    text in module2 modified

--- modules/module2/src/components/App.js
+++ modules/module2/src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 export default ({ name = "<module_name>" }) => {
   return (
     <div>
-      [Module 2] <b>remote</b> {name} React micro app. deployed
+      [Module 2] <b>remote</b> {name} React micro app
     </div>
   );
 };
